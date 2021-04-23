7ec8893634cdb32b6eaa3bc12e12d31c5a244670
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2021-04-23 16:22:25 +0900

    day 3

--- sources/hangman_old.py
+++ sources/hangman_old.py
@@ -4,26 +4,26 @@ def get_word():
     WORDS = "turaco shoebill tropicbird eagle crane peafowl dove \
             hummingbird puffin cassowary lorikeet flamingo penguin \
             heron booby owl toucan".split()
-    index = random.randint(0, len(WORDS)-1)
-    return WORDS[index]
+    offset = random.randint(0, len(WORDS)-1)
+    return WORDS[offset]
 
 def unmask_word(word, letters):
     st = ""
     for c in word:
-        tmp = "_"
         for l in letters:
             if c == l:
-                tmp = c
-        st = st + tmp
+                st = st + c
+                break
+        else:
+            st = st + "_"
     return st
 
 def is_correct(secretword, letters):
     for c in secretword:
-        is_covered = False
         for l in letters:
             if c == l:
-                is_covered = True
-        if is_covered == False:
+                break
+        else:
             return False
     return True
     
@@ -31,18 +31,21 @@ def input_letter():
     while True:
         print("英文字を1字入力")
         st = input()
-        if len(st) == 1 and st in 'abcdefghijklmnopqrstuvwxyz':
-            return st
+        st = st.lower()
+        if len(st) == 1:
+            if st in 'abcdefghijklmnopqrstuvwxyz':
+                break
+    return st
 
 life = 5
-letters = ""
+inputLetters = ""
 secret = get_word()
-st = unmask_word(secret, letters)
+st = unmask_word(secret, inputLetters)
 print("life:", life)
 print(st)
-while is_correct(secret, letters) == False:
-    letters = letters + input_letter()
-    st_next = unmask_word(secret, letters)
+while is_correct(secret, inputLetters) == False:
+    inputLetters = inputLetters + input_letter()
+    st_next = unmask_word(secret, inputLetters)
     print(st_next)
     if st == st_next:
         life = life - 1
