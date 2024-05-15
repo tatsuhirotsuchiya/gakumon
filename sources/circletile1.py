9df5166c5db9b9ade5a5af136a870731bfd93a51
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2024-05-15 11:41:43 +0900

    day 5
    Updates for day 5

---
+++ sources/circletile1.py
@@ -0,0 +1,16 @@
+import pyxel
+import random
+
+pyxel.init(160, 160)
+
+# 画面の塗りつぶし
+pyxel.cls(1)
+
+for y in range(0, 161, 20):
+    for x in range(0, 161, 20):
+        # ここに処理を追加して
+        # 課題を完成させる
+        pass
+
+# 画面表示
+pyxel.show()
--- sources/pyxel_test1.py
+++ sources/pyxel_test1.py
@@ -4,15 +4,15 @@ pyxel.init(160, 120)
 x = 0
 Delta = 10
 
+
 def update():
     global x
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
     x = (x + Delta) % 160
 
+
 def draw():
-    global x
     pyxel.cls(0)
     pyxel.rect(x, 10, x+10, 20, 11)
+
 
 pyxel.run(update, draw)
